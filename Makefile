f6ca5bae3bb58472e661eddc06f91584f0c737e2
Vladyslav Pushkarov <vpushkar@1-C-4.42heilbronn.de>
2025-03-11 16:51:10 +0100

    Created 2 new functions. Add these functions to Makefile and libft.h.

--- Makefile
+++ Makefile
@@ -6,15 +6,15 @@
 #    By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/03/11 14:13:04 by vpushkar          #+#    #+#              #
-#    Updated: 2025/03/11 16:16:04 by vpushkar         ###   ########.fr        #
+#    Updated: 2025/03/11 16:37:03 by vpushkar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = libft.a
 CC = cc
 CFLAGS = -Werror -Wall -Wextra
-SRC = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_atoi.c ft_strlen.c ft_strncmp.c \
-	ft_tolower.c ft_toupper.c
+SRC = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
+	ft_atoi.c ft_strlen.c ft_strncmp.c ft_tolower.c ft_toupper.c
 OBJ = $(SRC:.c=.o)
 INC = libft.h
 RM = rm -f
--- ft_isprint.c
+++ ft_isprint.c
@@ -1,28 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/10 17:11:51 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/03/11 16:36:37 by vpushkar         ###   ########.fr       */
+/*   Created: 2025/03/11 16:31:50 by vpushkar          #+#    #+#             */
+/*   Updated: 2025/03/11 16:41:04 by vpushkar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stddef.h>
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-int		ft_atoi(const char *str);
-size_t	ft_strlen(const char *s);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-
-#endif
+// #include <stdio.h>
+// #include <ctype.h>
+int	ft_isprint(int c)
+{
+	if (c >= 32 && c <= 126)
+		return (1);
+	return (0);
+}
+// int	main()
+// {
+// 	printf("%d\n", ft_isprint(000));
+// 	printf("%d\n", isprint(000));
+// }
