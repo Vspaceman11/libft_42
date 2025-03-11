8c9d1cae15b8dcf462e550b3cb17c935f2ad2656
Vladyslav Pushkarov <vpushkar@1-C-4.42heilbronn.de>
2025-03-11 18:02:02 +0100

    Created ft_strrchr.c and added it into libft and Makefile

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 17:11:51 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/03/11 17:36:31 by vpushkar         ###   ########.fr       */
+/*   Updated: 2025/03/11 17:55:23 by vpushkar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 int		ft_atoi(const char *str);
 size_t	ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
