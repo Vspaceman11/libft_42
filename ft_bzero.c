c5a34dc9c821670b05b5122a11bef8597618c25a
Vladyslav Pushkarov <vpushkar@1-C-4.42heilbronn.de>
2025-03-19 17:38:50 +0100

    Add comments to every function

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,14 +6,21 @@
 /*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:22:28 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/03/13 15:37:56 by vpushkar         ###   ########.fr       */
+/*   Updated: 2025/03/19 17:38:01 by vpushkar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-// #include <stdio.h>
-// #include <string.h>
-
+/**
+ * @brief Sets the first @p n bytes of the memory area
+ * pointed to by @p s to zero.
+ *
+ * This function writes zeroes to the first @p n bytes of the memory area
+ * pointed to by @p s. It does not return anything.
+ *
+ * @param s The memory area to fill with zeroes.
+ * @param n The number of bytes to set to zero.
+ */
 void	ft_bzero(void *s, size_t n)
 {
 	size_t			i;
@@ -27,21 +34,3 @@ void	ft_bzero(void *s, size_t n)
 		i++;
 	}
 }
-// int	main(void)
-// {
-// 	char arr1[11] = "HelloWorld";
-// 	char arr2[11] = "HelloWorld";
-
-// 	// Printing before bzero
-// 	printf("Before ft_bzero: arr1 = %s\n", arr1);
-// 	printf("Before bzero: arr2 = %s\n", arr2);
-
-// 	// My ft_bzero
-// 	ft_bzero(arr1, 5);  // Zeroing first 5 elements
-// 	printf("After ft_bzero: arr1 = %s\n", arr1); // Output empty string
-
-// 	// Original bzero
-// 	bzero(arr2, 5);  // Zeroing first 5 elements
-// 	printf("After bzero: arr2 = %s\n", arr2); // Output empty string
-// 	return 0;
-// }
--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,22 +6,25 @@
 /*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 16:40:42 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/03/11 12:59:27 by vpushkar         ###   ########.fr       */
+/*   Updated: 2025/03/19 17:35:32 by vpushkar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Checks if a character is an alphabetic letter.
+ *
+ * This function checks if the character @p c is either a lowercase letter
+ * (between 'a' and 'z') or an uppercase letter (between 'A' and 'Z').
+ * If @p c is an alphabetic letter, the function returns 1; otherwise, it
+ * returns 0.
+ *
+ * @param c The character to check.
+ *
+ * @return 1 if @p c is an alphabetic letter, 0 otherwise.
+ */
 int	ft_isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	return (0);
 }
-// #include <stdio.h>
-// #include <unistd.h>
-// #include <ctype.h>
-// int	main()
-// {
-// 	int num = ft_isalpha('a');
-// 	printf("%d\n", num);
-// 	printf("%d", isalpha('a'));
-// }
