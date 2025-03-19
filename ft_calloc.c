b76205d114f906e850c18fe27327805975d101f8
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2025-03-19 17:38:50 +0100

    Add comments to every function

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,14 +6,23 @@
 /*   By: vpushkar <vpushkar@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:41:43 by vpushkar          #+#    #+#             */
-/*   Updated: 2025/03/14 18:57:17 by vpushkar         ###   ########.fr       */
+/*   Updated: 2025/03/19 17:36:28 by vpushkar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-// #include <stdio.h>
-// #include <string.h>
-
+/**
+ * @brief Allocates memory and initializes it to zero.
+ *
+ * This function allocates memory for an array of @p count elements, each of
+ * size @p size, and initializes all bytes in the allocated memory to zero.
+ * If memory allocation fails, it returns NULL.
+ *
+ * @param count The number of elements to allocate.
+ * @param size The size of each element.
+ *
+ * @return A pointer to the allocated memory, or NULL if the allocation fails.
+ */
 void	*ft_calloc(size_t count, size_t size)
 {
 	size_t			i;
@@ -30,34 +39,3 @@ void	*ft_calloc(size_t count, size_t size)
 	}
 	return (p);
 }
-// int main(void)
-// {
-// 	size_t count = 5;
-// 	size_t size = sizeof(int);
-
-// 	// My ft_calloc
-// 	int *arr1 = (int *)ft_calloc(count, size);
-// 	if (arr1 == NULL) {
-// 		printf("Memory allocation error with ft_calloc\n");
-// 		return 1;
-// 	}
-
-// 	// Oroginal calloc
-// 	int *arr2 = (int *)calloc(count, size);
-// 	if (arr2 == NULL) {
-// 		printf("Memory allocation error with calloc\n");
-// 		free(arr1);
-// 		return 1;
-// 	}
-
-// 	// Check is every element in array is 0
-// 	for (size_t i = 0; i < count; i++) {
-// 		printf("ft_calloc arr1[%zu] = %d\n", i, arr1[i]);
-// 		printf("calloc arr2[%zu] = %d\n", i, arr2[i]);
-// 	}
-
-// 	free(arr1);
-// 	free(arr2);
-
-// 	return 0;
-// }
